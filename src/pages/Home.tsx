b16229ee6f7d70a8a05f700f518c14356e27417a
jadonus <jadongearhart@gmail.com>
2023-09-24 15:33:12 -0400

    committt

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -112,7 +112,7 @@ const ExploreContainer: React.FC<ContainerProps> = () => {
   return (
     <>
       <IonPage>
-        <IonHeader collapse="condense">
+        <IonHeader>
           <IonToolbar>
             <IonTitle size="large">Dashboard</IonTitle>
             <IonButtons slot="end">
