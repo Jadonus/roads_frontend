264b148cb8da1082b1a5b4796f2044b5b931e544
jadonus <jadongearhart@gmail.com>
2023-11-01 16:58:13 -0400

    okie achie

--- src/pages/appicon.tsx
+++ src/pages/appicon.tsx
@@ -8,7 +8,9 @@ import {
   IonHeader,
   IonToolbar,
   IonItem,
-  IonCheckbox,
+  IonRadioGroup,
+  IonRadio,
+  IonList,
 } from "@ionic/react";
 import { AppIcon } from "@capacitor-community/app-icon";
 function Appicon() {
@@ -22,10 +24,45 @@ function Appicon() {
           <IonButtons slot="start">
             <IonBackButton></IonBackButton>
           </IonButtons>
-          <IonTitle>App Icon</IonTitle>
+          <IonTitle size="large">App Icon</IonTitle>
         </IonToolbar>
-        <IonButton onClick={() => changeIcon("neumorph")}>Change</IonButton>
       </IonHeader>
+      <IonContent>
+        <IonList inset>
+          <IonRadioGroup
+            onIonChange={(e) => changeIcon(e.detail.value)}
+            allowEmptySelection={false}
+          >
+            <IonItem>
+              <img
+                className="ion-padding"
+                src="/roads.png"
+                width="100"
+                alt="default icon"
+              />
+              <IonRadio value="default">Default</IonRadio>
+            </IonItem>
+            <IonItem>
+              <img
+                className="ion-padding"
+                src="/rounded.png"
+                width="100"
+                alt="neumorph icon"
+              />
+              <IonRadio value="neumorph">Neumorph</IonRadio>
+            </IonItem>
+            <IonItem>
+              <img
+                className="ion-padding"
+                src="/dsf.png"
+                width="100"
+                alt="neumorph icon"
+              />
+              <IonRadio value="classic">Classic</IonRadio>
+            </IonItem>
+          </IonRadioGroup>{" "}
+        </IonList>
+      </IonContent>
     </IonPage>
   );
 }
